b42f510a3bf063289c158eb162d0231cade41fb7
“nahuel” <n.gorosito@yahoo.com>
2023-06-15 11:17:56 -0400

    Add title to the HTML document & increase board width by 100px

--- src/js/move.js
+++ src/js/move.js
@@ -3,7 +3,7 @@ const board = document.querySelector(".board");
 
 function noWallClose(x, y, type = "vertical") {
   const indexes = Array.from(
-    Array(Math.round(pacman.clientWidth * 0.75)),
+    Array(Math.round(pacman.clientWidth * 0.5)),
     (n, idx) => idx
   );
   const match = indexes.find((idx) => {
@@ -18,7 +18,9 @@ function moveUp(currMove) {
   let inProgress = true;
   const { x: pacX, y: pacY } = pacman.getBoundingClientRect();
   // const closest = document.elementFromPoint(pacX + 20, pacY - 15);
-  if (pacman.offsetTop > 26 && noWallClose(pacX, pacY - 15)) {
+  // const keepMoving = !closest.classList.contains("wall");
+  const keepMoving = noWallClose(pacX, pacY - 15);
+  if (pacman.offsetTop > 26 && keepMoving) {
     pacman.style.top = +pacman.style.top.replace(/\D/g, "") - 10 + "px";
   } else {
     cancelAnimationFrame(currMove);
@@ -31,7 +33,10 @@ function moveDown(currMove) {
   let inProgress = true;
   const { x: pacX, bottom: pacY } = pacman.getBoundingClientRect();
   const bottomEdge = board.clientHeight - pacman.clientHeight - 6;
-  if (pacman.offsetTop < bottomEdge && noWallClose(pacX, pacY + 15)) {
+  // const closest = document.elementFromPoint(pacX, pacY + 15);
+  // const keepMoving = !closest.classList.contains("wall");
+  const keepMoving = noWallClose(pacX, pacY + 15);
+  if (pacman.offsetTop < bottomEdge && keepMoving) {
     pacman.style.top = +pacman.style.top.replace(/\D/g, "") + 10 + "px";
   } else {
     cancelAnimationFrame(currMove);
@@ -42,15 +47,18 @@ function moveDown(currMove) {
 
 function moveLeft(currMove) {
   let inProgress = true;
-  const leftEdge = board.offsetLeft + 18;
+  const leftEdge = board.offsetLeft + 24;
   const {
     x: pacX,
     y: pacY,
     bottom: pacBottom,
   } = pacman.getBoundingClientRect();
   // const closest = document.elementFromPoint(pacX - 28, pacY - 2);
+  // const keepMoving = !closest.classList.contains("wall");
   const keepMoving =
-    noWallClose(pacX - 28, pacY - 2) && noWallClose(pacX - 28, pacBottom + 2);
+    noWallClose(pacX - 28, pacY - 2) &&
+    noWallClose(pacX - 28, pacBottom + 2) &&
+    noWallClose(pacX - 28, pacY + pacman.clientHeight / 2);
   if (pacX > leftEdge && keepMoving) {
     pacman.style.left = +pacman.style.left.replace(/\D/g, "") - 10 + "px";
   } else {
@@ -71,9 +79,11 @@ function moveRight(currMove) {
     bottom: pacBottom,
   } = pacman.getBoundingClientRect();
   // const closest = document.elementFromPoint(pacRight + 28, pacY - 2);
+  // const keepMoving = !closest.classList.contains("wall");
   const keepMoving =
     noWallClose(pacRight, pacY - 2) &&
-    noWallClose(pacRight, pacBottom + 2);
+    noWallClose(pacRight, pacBottom + 2) &&
+    noWallClose(pacRight, pacY + pacman.clientHeight / 2);
   if (pacX < rightEdge && keepMoving) {
     pacman.style.left = +pacman.style.left.replace(/\D/g, "") + 10 + "px";
   } else {
